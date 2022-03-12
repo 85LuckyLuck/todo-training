f9de265c524442ac45f67605c923b1df4dbca347
85Luckyluck <lukaszhytros@interia.eu>
2022-03-12 19:22:25 +0100

    zrobienie strony dynamicznej z wykl. 1 zad.dostkow

--- src/app/pages/employee-details.page.ts
+++ src/app/pages/employee-details.page.ts
@@ -1,5 +1,20 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { 
+    Component,
+     ViewEncapsulation,
+     ChangeDetectionStrategy, 
+     OnInit
+    } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-@Component({ selector: 'lib-employee-details-page', templateUrl: './employee-details.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
-export class EmployeeDetailsPage {
+@Component({ 
+    selector: 'lib-employee-details-page', 
+    templateUrl: './employee-details.page.html', 
+    encapsulation: ViewEncapsulation.None,
+     changeDetection: ChangeDetectionStrategy.OnPush,
+     })
+export class EmployeeDetailsPage  {
+   
+    constructor(private _activateRoute:ActivatedRoute) {}
+name$= this._activateRoute.params;
+
 }
