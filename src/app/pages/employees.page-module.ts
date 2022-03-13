7985c48de7be2f1be8dfae81c1a76909f973d0b3
85Luckyluck <lukaszhytros@interia.eu>
2022-03-13 18:00:05 +0100

    uzycie smart comp. na page emploees

--- src/app/pages/employees.page-module.ts
+++ src/app/pages/employees.page-module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { EmployeesPage } from './employees.page';
+import { TeamListComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/team-list.component-module';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
@@ -9,7 +10,9 @@ import { EmployeesPage } from './employees.page';
           path: '',
           component: EmployeesPage,
         }
-      ])],
+      ]),
+  TeamListComponentModule
+],
   	declarations: [EmployeesPage],
   	providers: [],
   	exports: [] })
