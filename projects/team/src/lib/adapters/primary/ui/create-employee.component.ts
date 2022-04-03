f6e3f0b625027b4a50e6d803c0fb4ea1b943abb6
85Luckyluck <lukaszhytros@interia.eu>
2022-04-03 16:28:52 +0200

    formu.OnazwieCreateEmployeeIproperitisZEmployeeDTO

--- projects/team/src/lib/adapters/primary/ui/create-employee.component.ts
+++ projects/team/src/lib/adapters/primary/ui/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({ selector: 'lib-create-employee', templateUrl: './create-employee.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class CreateEmployeeComponent {
+  readonly createEmployee: FormGroup = new FormGroup({name: new FormControl(), bio: new FormControl(), imageUrl: new FormControl(), departmentName: new FormControl(), employeeCount: new FormControl()});
 }
