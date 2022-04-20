da366fa2af9525b1425046dadf37f8560b577bf1
85Luckyluck <lukaszhytros@interia.eu>
2022-04-20 11:34:19 +0200

    Podpięcie resolver w page module

--- src/app/pages/employee-details.page-module.ts
+++ src/app/pages/employee-details.page-module.ts
@@ -4,16 +4,22 @@ import { RouterModule } from '@angular/router';
 import { EmployeeDetailsPage } from './employee-details.page';
 import { EmployeeDetailComponentModule } from '../../../projects/team/src/lib/adapters/primary/ui/employee-detail.component-module';
 import { FirebaseEmployeesServiceModule } from '../../../projects/team/src/lib/adapters/secondary/infrastructure/firebase-employees.service-module';
+import { EmployeeIdResolverModule } from '@team';
+import { EmployeeIdResolver } from 'projects/team/src/lib/adapters/primary/ui/employee-id.resolver';
 
 @NgModule({ imports: [CommonModule, 
       RouterModule.forChild([
         {
           path: '',
           component: EmployeeDetailsPage,
+          resolve: {
+            employeeId: EmployeeIdResolver,
+          }
         }
       ]),
   EmployeeDetailComponentModule,
   FirebaseEmployeesServiceModule,
+  EmployeeIdResolverModule,
 ],
   	declarations: [EmployeeDetailsPage],
   	providers: [],
